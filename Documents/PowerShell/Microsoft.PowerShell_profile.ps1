ad500f9ac598aaaf5d67d9e747843b5dbfcb3af9
keta4 <82580781+keta4@users.noreply.github.com>
2022-05-19 12:17:45 +0900

    Initial commit

---
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,9 @@
+Import-Module -Name Terminal-Icons
+Import-Module posh-git
+$env:POSH_GIT_ENABLED = $true
+# oh-my-posh init pwsh --config $env:LOCALAPPDATA\Programs\oh-my-posh\themes\paradox.omp.json | Invoke-Expression
+Invoke-Expression (&starship init powershell)
+
+Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
+Set-PSReadLineOption -PredictionSource History
+# Set-PSReadlineOption -Colors @{ Prediction = 'DarkGreen' }
