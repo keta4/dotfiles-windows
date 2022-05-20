dd86fbd47ce13d40a704566661fa165bc1ca9b04
keta4 <82580781+keta4@users.noreply.github.com>
2022-05-21 05:19:57 +0900

    History -PredictionViewStyle ListView

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -6,7 +6,8 @@ $env:POSH_GIT_ENABLED = $true
 Invoke-Expression (&starship init powershell)
 
 Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
-Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionSource History -PredictionViewStyle ListView
+Set-PSReadLineKeyHandler -Key "Ctrl+f" -Function ForwardWord
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 
 # Set-PSReadlineOption -Colors @{ Prediction = 'DarkGreen' }
