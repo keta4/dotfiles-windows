716f3e613aebc9cecfc5c29eaa676e8964ff19b6
keta4 <82580781+keta4@users.noreply.github.com>
2022-06-12 20:17:08 +0900

    updated

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,4 @@
 Import-Module -Name Terminal-Icons
-Import-Module posh-git
-$env:POSH_GIT_ENABLED = $true
 # oh-my-posh init pwsh --config $env:LOCALAPPDATA\Programs\oh-my-posh\themes\paradox.omp.json | Invoke-Expression
 Invoke-Expression (&starship init powershell)
 
