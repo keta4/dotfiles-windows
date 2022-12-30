356ed30e39b80dc5892085f5cf3b6f81c5653f46
keta4 <82580781+keta4@users.noreply.github.com>
2022-12-31 04:10:36 +0900

    updated

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,10 +3,3 @@ Import-Module posh-git
 $env:POSH_GIT_ENABLED = $true
 
 Invoke-Expression (&starship init powershell)
-
-Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
-Set-PSReadLineOption -PredictionSource History -PredictionViewStyle ListView
-Set-PSReadLineKeyHandler -Key "Ctrl+f" -Function ForwardWord
-Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
-
-# Set-PSReadlineOption -Colors @{ Prediction = 'DarkGreen' }
