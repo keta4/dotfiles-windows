327fc5d2136c192616642e8203c3a3a09308bcfe
keta4 <82580781+keta4@users.noreply.github.com>
2022-12-31 04:25:47 +0900

    updated

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -5,3 +5,4 @@ $env:POSH_GIT_ENABLED = $true
 Invoke-Expression (&starship init powershell)
 
 Set-PSReadLineOption -PredictionSource History -PredictionViewStyle ListView
+Set-PSReadLineKeyHandler -Key "Ctrl+f" -Function ForwardWord
